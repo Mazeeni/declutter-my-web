fa3f3995960b0cb504ca308b3a6f6bcbe9da653d
Hussein, Mazen <mah319@ic.ac.uk>
2021-09-04 17:03:41 +0100

    Add filter popup page

--- src/js/background.js
+++ src/js/background.js
@@ -13,3 +13,14 @@ browser.menus.create({
     "video",
   ],
 });
+
+browser.menus.onClicked.addListener(async (info, tab) => {
+  popupParameters = {
+    tabId: tab.id,
+    frameId: info.frameId,
+    targetElementId: info.targetElementId,
+  };
+  browser.pageAction.show(tab.id);
+  await browser.pageAction.openPopup();
+  await browser.pageAction.hide(tab.id);
+});
