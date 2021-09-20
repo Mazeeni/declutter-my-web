14e8fa7cb73c8af6a4cc966273bc8366c7db2d7e
Hussein, Mazen <mah319@ic.ac.uk>
2021-09-20 17:41:41 +0100

    Add pushing class names to profile

--- src/js/filterPopup.js
+++ src/js/filterPopup.js
@@ -11,6 +11,9 @@ var port = 0;
 var currParentIndex = -1;
 var parentElemsHTML = [];
 var parentElemsClassLists = [];
+var tab;
+var profileGroupName;
+var allGroupProfiles;
 
 /*
  * Injects content script into page.
@@ -20,6 +23,16 @@ var parentElemsClassLists = [];
 async function onLoad() {
   const filterParams = await browser.runtime.sendMessage("getFilterParams");
   let { tabId, frameId, targetElementId } = filterParams;
+
+  const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  tab = tabs[0];
+  var url = tab.url;
+
+  const tabHostname = new URL(url).hostname.replace(/^(www\.)/, "");
+  profileGroupName = "profilesFor" + tabHostname;
+
+  const allProfiles = await browser.storage.local.get(profileGroupName);
+  allGroupProfiles = allProfiles[Object.keys(allProfiles)[0]];
 
   await browser.tabs.executeScript(tabId, {
     frameId,
@@ -87,19 +100,11 @@ function initialiseTargetElements(msgParentElems, msgElemsClassLists) {
 }
 
 async function initialiseAddToProfileBtns() {
-  const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-  var url = tabs[0].url;
-
-  const tabHostname = new URL(url).hostname.replace(/^(www\.)/, "");
-  const profileGroupName = "profilesFor" + tabHostname;
-
-  const allProfiles = await browser.storage.local.get(profileGroupName);
-  const profilesMatchingHostname = allProfiles[Object.keys(allProfiles)[0]];
   const res = [];
 
-  for (const key in profilesMatchingHostname) {
-    if (url.includes(profilesMatchingHostname[key].url)) {
-      res.push(profilesMatchingHostname[key]);
+  for (const key in allGroupProfiles) {
+    if (tab.url.includes(allGroupProfiles[key].url)) {
+      res.push(allGroupProfiles[key]);
     }
   }
 
@@ -107,8 +112,27 @@ async function initialiseAddToProfileBtns() {
   for (var i = 0; i < res.length; i++) {
     const addBtn = document.createElement("button");
     addBtn.innerText = "Add to profile: " + res[i].name;
+
+    const profName = res[i].name;
+    addBtn.addEventListener("click", () =>
+      addSelectedClassesToProfile(profName)
+    );
     div.appendChild(addBtn);
   }
+}
+
+async function addSelectedClassesToProfile(profName) {
+  const blockedClasses = allGroupProfiles[profName].blockedClasses;
+  blockedClasses.push(...Array.from(selectedClasses));
+  allGroupProfiles[profName].blockedClasses = blockedClasses;
+  browser.storage.local.set({
+    [profileGroupName]: allGroupProfiles,
+  });
+  selectedClasses.clear();
+  updateSelectedClasses();
+
+  // display valid message
+  document.querySelector("#successMessage").classList.remove("hidden");
 }
 
 function addListenerHighlightElem(elem) {
